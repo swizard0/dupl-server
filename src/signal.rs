33b92b03afe02cdb6100baeb471ed064e848d680
Alexey Voznyuk <me@swizard.info>
2015-12-03 05:57:06 +0300

    retry while TooBusy when processing Ctrl+C

--- src/signal.rs
+++ src/signal.rs
@@ -10,16 +10,22 @@ pub fn term_on_signal(external_zmq_addr: &str) {
         let mut zmq_ctx = zmq::Context::new();
         let mut sock = zmq_ctx.socket(zmq::REQ).map_err(|e| ZmqError::Socket(e)).unwrap();
         sock.connect(&external_zmq_addr_owned).map_err(|e| ZmqError::Connect(external_zmq_addr_owned.clone(), e)).unwrap();
-        let packet: Req<String> = Req::Terminate;
-        let required = packet.encode_len();
-        let mut load_msg = zmq::Message::with_capacity(required).map_err(|e| ZmqError::Message(e)).unwrap();
-        packet.encode(&mut load_msg);
-        sock.send_msg(load_msg, 0).map_err(|e| ZmqError::Send(e)).unwrap();
         println!(" ;; SIGINT received, terminating server...");
-        let reply_msg = sock.recv_msg(0).map_err(|e| ZmqError::Recv(e)).unwrap();
-        match Rep::<String>::decode(&reply_msg).unwrap() {
-            (Rep::TerminateAck, _) => (),
-            (other, _) => panic!("unexpected reply for terminate: {:?}", other),
+        loop {
+            let packet: Req<String> = Req::Terminate;
+            let required = packet.encode_len();
+            let mut load_msg = zmq::Message::with_capacity(required).map_err(|e| ZmqError::Message(e)).unwrap();
+            packet.encode(&mut load_msg);
+            sock.send_msg(load_msg, 0).map_err(|e| ZmqError::Send(e)).unwrap();
+            let reply_msg = sock.recv_msg(0).map_err(|e| ZmqError::Recv(e)).unwrap();
+            match Rep::<String>::decode(&reply_msg).unwrap() {
+                (Rep::TerminateAck, _) => break,
+                (Rep::TooBusy, _) => {
+                    println!("  ;;; too busy, retrying in 250 ms...");
+                    ::std::thread::sleep(::std::time::Duration::from_millis(250));
+                },
+                (other, _) => panic!("unexpected reply for terminate: {:?}", other),
+            }
         }
     });
 }
